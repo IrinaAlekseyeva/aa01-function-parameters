bd6d7bfff91a55331e77cc91352bc6d420c9ca39
cmadden1986 <madden.charles@gmail.com>
2024-06-08 16:11:49 -0500

    2nd problem

--- problems/02-be-cool.js
+++ problems/02-be-cool.js
@@ -4,12 +4,16 @@ returns a sentence saying that the given string "is cool!". For example, if
 the first argument to beCool is "Mary", then the function should return
 "Mary is cool!"
 */
+function beCool(str){
+  let outcome = (str + " is cool!");
 
-// Your code here 
+  return outcome;
+}
+console.log(beCool("Music"));
 
-// console.log(beCool("Music"));      // "Music is cool!"
-// console.log(beCool("JavaScript")); // "JavaScript is cool!"
-// console.log(beCool("The world"));  // "The world is cool!"
+ console.log(beCool("Music"));      // "Music is cool!"
+ console.log(beCool("JavaScript")); // "JavaScript is cool!"
+ console.log(beCool("The world"));  // "The world is cool!"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = beCool;
